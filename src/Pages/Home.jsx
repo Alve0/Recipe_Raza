39d7caaaf4cdced53bc9890530646d88a06af345
Alve <gamingalve@gmail.com>
2025-06-01 16:25:04 +0600

    working

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import { createNightowl } from "@bufferhead/nightowl";
 import { AuthContext } from "../Provider/AuthProvider";
 import Loading from "../Components/login_reginster/Loading";
 
-export const url = "https://assignment-10-back-end-beta.vercel.app";
+export const url = "https://trying-againg.vercel.app";
 // export const url = " http://localhost:3000";
 
 function Home() {
